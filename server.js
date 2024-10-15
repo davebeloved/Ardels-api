7e6f54a101c9c706ba612515d4b1fde3a5a982eb
davebeloved <idadakpoyedavidson@gmail.com>
2024-10-15 11:02:31 +0100

    updated

--- server.js
+++ server.js
@@ -54,13 +54,13 @@ app.use(
 // Session Configuration
 app.use(
   session({
-    secret: process.env.SESSION_SECRET, // Secret key for signing the session ID
+    secret: process.env.SESSION_SECRET,
     resave: false, // Prevent resaving session if unmodified
     saveUninitialized: true, // Save new sessions even if they're not modified
     cookie: {
       maxAge: 1000 * 60 * 15, // Session expiry time: 15 minutes
       secure: true,
-      // httpOnly: true,
+      httpOnly: true,
       sameSite: "None",
     },
     store: MongoStore.create({
